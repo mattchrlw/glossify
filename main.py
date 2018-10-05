5d7616f9ea0dc2ecd8fa26d5d032c1cf444038e7
Matthew Low <mattchrlw@gmail.com>
2018-10-05 23:00:56 +1000

    Update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ with open('output.txt', 'w') as output:
         for i, row in enumerate(reader):
             if i > 0:
                 output.write('\\newglossaryentry{' + row[0] + '}' + '\n{\n')
-                output.write('\tname=' + row[0] + ',\n')
-                output.write('\tdescription=' + row[2] + ',\n')
-                output.write('\tsymbol=$' + row[1] + '$\n}\n\n')
-                print(row[0], row[1], row[2])
+                output.write('\tname={' + row[0] + '},\n')
+                output.write('\tdescription={' + row[2] + '},\n')
+                output.write('\tsymbol={$' + row[1] + '$}\n}\n\n')
